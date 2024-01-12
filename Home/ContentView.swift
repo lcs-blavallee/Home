3d9856cc9a19a4de8aad373f66716ab27092d9b0
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-01-12 09:55:05 -0500

    Finished gradient, got it pretty accurate

--- Home/ContentView.swift
+++ Home/ContentView.swift
@@ -10,17 +10,33 @@ import SwiftUI
 struct ContentView: View {
     
     //MARK: Stored properties
-    let firstGradientColors = Gradient(colors: [Color.lightBlue, Color.mediumBlue, Color.darkerBlue, Color.deepBlue, Color.gray, Color.darkYellow])
+    let firstGradientColors = Gradient(colors: [Color.lightBlue, Color.mediumBlue, Color.darkerBlue, Color.deepBlue, Color.clear])
+    let secondGradientColors = Gradient(colors: [Color.darkYellow, Color.darkYellow, Color.gray, Color.darkestBlue, Color.clear])
     
     //MARK: Computed properties
     var body: some View {
         ZStack {
             
             //Background
-            LinearGradient(
+            
+            
+            //First Gradient
+            
+            RadialGradient(
                 gradient: firstGradientColors,
-                startPoint: .topLeading,
-                endPoint: .bottomTrailing
+                center: .topLeading,
+                startRadius: 0,
+                endRadius: 750
+            )
+            .ignoresSafeArea()
+            
+            //Second Gradient
+            
+            RadialGradient(
+                gradient: secondGradientColors,
+                center: UnitPoint(x: 1.2, y: 1.1),
+                startRadius: 0,
+                endRadius: 750
             )
             .ignoresSafeArea()
             
